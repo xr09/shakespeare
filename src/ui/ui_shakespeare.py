d18e1605e6e6b67575bb1c6c11e6d0e14cb404f0
dayron <dyro.ar@gmail.com>
2018-09-11 14:44:39 -0400

    already operational

--- src/ui/ui_shakespeare.py
+++ src/ui/ui_shakespeare.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 
-from PyQt5.QtWidgets import (QMainWindow, QApplication, QSystemTrayIcon, QAction, qApp, QMenu)
-from PyQt5.QtGui import (QKeySequence, QIcon)
+from PyQt5.QtWidgets import (QMainWindow, QApplication, QSystemTrayIcon,
+    QAction, qApp, QMenu, QMessageBox)
+from PyQt5.QtGui import (QKeySequence, QIcon, QColor)
 from PyQt5.QtCore import Qt
 from .form_ui import Ui_MainWindow
 from core.shakespeare import Shakespeare
@@ -18,12 +19,26 @@ class UI_Shakespeare(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.result_TextEdit.setFontPointSize(10)
         self.actionQuit.setShortcut(QKeySequence('Ctrl+Q'))
-        self.actionHide = QAction(self, triggered=self.hide)
-        self.actionHide.setShortcut(Qt.Key_Escape)
+        self.actionQuit.triggered.connect(self.on_actionQuit_triggered)
         self.engine = Shakespeare()
         self.search_lineEdit.setFocus()
         self.setupSysTray()
         self.rightCorner()
+
+    def connectActions(self):
+        self.language_pushButton.triggered.connect(
+            self.on_language_pushButton_clicked)
+        self.search_lineEdit.textChanged.connect(
+            self.on_search_lineEdit_textChanged)
+        self.exact_checkBox.stateChanged.connect(
+            self.on_exact_checkBox_stateChanged)
+        self.searchtype_comboBox.currentIndexChanged.connect(
+            self.on_searchtype_comboBox_currentIndexChanged)
+        self.actionAbout.triggered.connect(self.on_actionAbout_triggered)
+        self.actionAbout_Qt.triggered.connect(self.on_actionAbout_Qt_triggered)
+
+    def testHide(self):
+        print("Hidding!")
 
     def rightCorner(self):
         widget = QApplication.desktop()
@@ -40,13 +55,14 @@ class UI_Shakespeare(QMainWindow, Ui_MainWindow):
     def setupSysTray(self):
         self.trayIcon = QSystemTrayIcon(QIcon(':/icons/imgs/shakespeare.png'))
         self.trayIcon.activated.connect(self.restore)
-        quitAction = QAction(QIcon(":/icons/imgs/quit.png"), "Quit", self, triggered=qApp.quit)
+        quitAction = QAction(QIcon(":/icons/imgs/quit.png"), "Quit", self,
+            triggered=qApp.quit)
         trayIconMenu = QMenu(self)
         trayIconMenu.addAction(quitAction)
         self.trayIcon.setContextMenu(trayIconMenu)
         self.trayIcon.show()
         # hide with Escape
-        # esc_shorcut = QtGui.QShortcut(
+        # esc_shorcut = QShortcut(
         #                               QKeySequence(
         #                               Qt.Key_Escape), self)
         # self.connect(esc_shorcut, SIGNAL("activated()"), self.hide)
@@ -58,82 +74,74 @@ class UI_Shakespeare(QMainWindow, Ui_MainWindow):
                 self.hide()
             else:
                 self.update()
-                self.show()
+                self.show()    
 
-    # @QtCore.pyqtSignature("")
-    # def on_clear_pushButton_clicked(self):
-    #     self.search()
+    def on_clear_pushButton_clicked(self):
+         self.search()
+    
+    def on_language_pushButton_clicked(self):
+        self.search()
 
-    # @QtCore.pyqtSignature("")
-    # def on_language_pushButton_clicked(self):
-    #     self.search()
+    def statusMessage(self, amount):
+        message = ('%i words found' % amount) if amount else 'word not found'
+        self.statusbar.showMessage(message)
 
-    # def statusMessage(self, amount):
-    #     message = ('%i words found' % amount) if amount else 'word not found'
-    #     self.statusbar.showMessage(message)
+    def renderText(self, result, reverse=False):
+        if reverse:
+            color1, color2 = 'Red', 'Blue'
+        else:
+            color1, color2 = 'Blue', 'Red'
+        for e, s in result:
+            self.result_TextEdit.setTextColor(QColor(color1))
+            self.result_TextEdit.insertPlainText(e)
+            self.result_TextEdit.setTextColor(QColor('Black'))
+            self.result_TextEdit.insertPlainText(' : ')
+            self.result_TextEdit.setTextColor(QColor(color2))
+            self.result_TextEdit.insertPlainText('  ' + s + '\n')
 
-    # def renderText(self, result, reverse=False):
-    #     if reverse:
-    #         color1, color2 = 'Red', 'Blue'
-    #     else:
-    #         color1, color2 = 'Blue', 'Red'
-    #     for e, s in result:
-    #         self.result_TextEdit.setTextColor(QtGui.QColor(color1))
-    #         self.result_TextEdit.insertPlainText(e)
-    #         self.result_TextEdit.setTextColor(QtGui.QColor('Black'))
-    #         self.result_TextEdit.insertPlainText(' : ')
-    #         self.result_TextEdit.setTextColor(QtGui.QColor(color2))
-    #         self.result_TextEdit.insertPlainText('  ' + s + '\n')
+    def search(self):
+        self.result_TextEdit.clear()
+        if not len(self.search_lineEdit.text()):
+            self.statusbar.clearMessage()
+            return
+        result = self.engine.search(self.search_lineEdit.text(),
+                                self.searchtype_comboBox.currentIndex(),
+                                self.exact_checkBox.isChecked(),
+                                self.language_pushButton.isChecked())
+        self.statusMessage(len(result))
+        if not result:
+            self.statusMessage(len(result))
+            return
+        self.renderText(result, self.language_pushButton.isChecked())
+    
+    def on_search_lineEdit_textChanged(self):
+        self.search()
+    
+    def on_exact_checkBox_stateChanged(self):
+        self.search()
+        self.searchtype_comboBox.setVisible(
+            not self.exact_checkBox.isChecked())
+    
+    def on_searchtype_comboBox_currentIndexChanged(self):
+        self.search()
+    
+    def on_actionQuit_triggered(self):
+        qApp.quit()
 
-    # def search(self):
-    #     self.result_TextEdit.clear()
-    #     if not self.search_lineEdit.text().length():
-    #         self.statusbar.clearMessage()
-    #         return
-    #     result = self.engine.search(self.search_lineEdit.text(),
-    #                             self.searchtype_comboBox.currentIndex(),
-    #                             self.exact_checkBox.isChecked(),
-    #                             self.language_pushButton.isChecked())
-    #     self.statusMessage(len(result))
-    #     if not result:
-    #         self.statusMessage(len(result))
-    #         return
-    #     self.renderText(result, self.language_pushButton.isChecked())
+    def on_actionAbout_triggered(self):
+        QMessageBox.about(self, u"About Shakespeare",
+        u"<h3>Shakespeare %s </h3>\
+        English/Spanish dictionary <br/>\
+        <hr>(C) Manuel E. Gutierrez 2007-2013 <br/>\
+         <br/>This program is licensed under the GPL v3<br />\
+         <br />\
+         http://github.org/xr09/shakespeare</p>" % VERSION)
 
-    # @QtCore.pyqtSignature("QString")
-    # def on_search_lineEdit_textChanged(self):
-    #     self.search()
+    def on_actionAbout_Qt_triggered(self):
+        QMessageBox.aboutQt(self)
 
-    # @QtCore.pyqtSignature('int')
-    # def on_exact_checkBox_stateChanged(self):
-    #     self.search()
-    #     self.searchtype_comboBox.setVisible(
-    #                                     not self.exact_checkBox.isChecked())
+    def closeEvent(self, event):
+        self.hide()
+        event.ignore()
 
-    # @QtCore.pyqtSignature('int')
-    # def on_searchtype_comboBox_currentIndexChanged(self):
-    #     self.search()
-
-    # @QtCore.pyqtSignature('')
-    # def on_actionQuit_triggered(self):
-    #     QtGui.qApp.quit()
-
-    # @QtCore.pyqtSignature('')
-    # def on_actionAbout_triggered(self):
-    #     QtGui.QMessageBox.about(self, u"About Shakespeare",
-    #     u"<h3>Shakespeare %s </h3>\
-    #     English/Spanish dictionary <br/>\
-    #     <hr>(C) Manuel E. Gutierrez 2007-2013 <br/>\
-    #      <br/>This program is licensed under the GPL v3<br />\
-    #      <br />\
-    #      http://github.org/xr09/shakespeare</p>" % unicode(VERSION))
-
-    # @QtCore.pyqtSignature('')
-    # def on_actionAbout_Qt_triggered(self):
-    #     QtGui.QMessageBox.aboutQt(self)
-
-    # @QtCore.pyqtSignature("")
-    # def closeEvent(self, event):
-    #     self.hide()
-    #     event.ignore()
 from . import res_rc
